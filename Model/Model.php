0adb3c7c22ca04757246bd517a0618a31a3946c1
Дарма Бороев <darmaboroev@MacBook-Pro-Darma.local>
2024-01-24 22:38:08 +0800

    init commit

---
+++ Model/Model.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Core\Model;
+
+use PDO;
+
+class Model
+{
+    protected static PDO $pdo;
+
+    public static function getPDO()
+    {
+        //shablon almost as odinochka because create only one object
+        if (isset(self::$pdo)) {
+            return self::$pdo;
+        }
+        self::$pdo = new PDO("pgsql:host=db;dbname=postgres", "dbuser", "dbpwd");
+
+        return self::$pdo;
+    }
+}
+
